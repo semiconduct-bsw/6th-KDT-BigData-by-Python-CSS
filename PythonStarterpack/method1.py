3374cd3391d62d125431391341525a4c01c7785a
배선우 <chem1bsw.fac@gmail.com>
2025-12-23 15:45:13 +0900

    Create method1.py

---
+++ PythonStarterpack/method1.py
@@ -0,0 +1,30 @@
+# 함수 선언
+def add10_20() : 
+    a = 10
+    b = 20
+    c = a + b
+    print(c)
+
+# 함수 호출
+add10_20()
+add10_20()
+add10_20()
+add10_20()
+add10_20()
+
+#함수선언 매개변수, 리턴
+# 1. 함수명
+# 2. 로직 설계(임의의 값으로 코드 작성)
+# 3. 매개변수
+# 4. 리턴
+
+def add_two_numbers(num1, num2) :
+    sum = num1 + num2
+    print(sum)
+
+# 함수 호출
+add_two_numbers(10, 20)
+add_two_numbers(20, 40)
+add_two_numbers(40, 80)
+add_two_numbers(80, 120)
+add_two_numbers(120, 200)
