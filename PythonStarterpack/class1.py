d9120e6bd7af65bd784a58a91cde4ce038d96318
배선우 <chem1bsw.fac@gmail.com>
2025-12-24 09:42:04 +0900

    Update class1.py

--- PythonStarterpack/class1.py
+++ PythonStarterpack/class1.py
@@ -1,14 +1,20 @@
-#기본 디폴트
 class Animal:
     #생성자
-    def __init__(self, type, age):
+    def __init__(self, type, age, name):
         self.type = type
         self.age = age
+        self.name = name
+    
+    #함수
+    def bark(self):
+        print(f'{self.name}이/가 짖습니다.')
 
-dog = Animal('강아지', 2)
-cat = Animal('고양이', 3)
+dog = Animal('강아지', 2, '어엉')
+cat = Animal('고양이', 3, '웅어')
 
 print(dog.type)
 print(dog.age)
-print(cat.type)
-print(cat.age)
+print(dog.name)
+
+dog.bark()
+cat.bark()
