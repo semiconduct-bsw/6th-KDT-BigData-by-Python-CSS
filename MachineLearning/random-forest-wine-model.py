74b7f45f27bb27fb00af5bce437ab4648002f4fe
배선우 <chem1bsw.fac@gmail.com>
2025-12-26 11:21:05 +0900

    Implement model saving, loading, and prediction
    Added model saving and loading functionality, and prediction for new input values.

--- MachineLearning/random-forest-wine-model.py
+++ MachineLearning/random-forest-wine-model.py
@@ -1,8 +1,11 @@
+import joblib
 from sklearn.datasets import load_wine
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import classification_report, accuracy_score
+from sklearn.metrics import classification_report, accuracy_score, ConfusionMatrixDisplay
+import matplotlib.pyplot as plt
+import numpy as np
 
 # 데이터셋 로드
 wine = load_wine()
@@ -29,3 +32,16 @@ cfr = classification_report(y_test, y_pred)
 # 평가 결과 출력
 print("정확도:", accuracy)
 print("\n결과 보고서:\n", cfr)
+
+# 모델 저장
+if accuracy > 0.85:
+    joblib.dump(model, f'model_pkl/wine_model_accuracy_{accuracy:.2f}.pkl')
+
+# 모델 로드
+model = joblib.load(f'model_pkl/wine_model_accuracy_{accuracy:.2f}.pkl')
+
+# 임의의 값으로 예축, 정규화된 값으로 예측
+X_new = np.array([[13.2, 2.7, 2.5, 15.6, 98, 3.3, 2.5, 15.6, 98, 3.3, 2.5, 15.6, 98]])
+X_new = scaler.transform(X_new)
+y_pred = model.predict(X_new)
+print("예측 결과:", y_pred)
