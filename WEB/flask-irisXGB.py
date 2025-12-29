bd421e27c3ee09e36519eb9a6f539147e696abbd
배선우 <chem1bsw.fac@gmail.com>
2025-12-29 11:21:41 +0900

    Create flask-irisXGB.py

---
+++ WEB/flask-irisXGB.py
@@ -0,0 +1,14 @@
+@app.route("/predict-iris", methods=['POST'])
+def predict_iris():
+    # 모델 로드
+    model = joblib.load('model/iris_xgb_model.pkl')
+    # 임의의 값 예측
+    sepal_length = request.json.get("sepal_length")
+    sepal_width = request.json.get("sepal_width")
+    petal_length = request.json.get("petal_length")
+    petal_width = request.json.get("petal_width")
+    test_data = np.array([[sepal_length, sepal_width, petal_length, petal_width]])
+    predict_class = model.predict(test_data)
+    print(predict_class)
+    print(f"Predicted class: {predict_class[0]}")
+    return str(predict_class[0])
