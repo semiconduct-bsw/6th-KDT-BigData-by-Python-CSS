2bdb9b6a2dced598c31090ebeac969f93df56ab6
배선우 <chem1bsw.fac@gmail.com>
2025-12-29 15:10:46 +0900

    Create flask-init.py

---
+++ WEB/flask-init.py
@@ -0,0 +1,7 @@
+from flask import Flask
+from . import iris_view, main_view, test
+
+def register_routes(app: Flask):
+    app.register_blueprint(iris_view.bp)
+    app.register_blueprint(main_view.bp)
+    app.register_blueprint(test.bp)
