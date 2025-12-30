bbf24a663d227c3f5ae0fed5b3ecf144302d5dc7
semiconduct-bsw <chem1bsw.fac@gmail.com>
2025-12-30 13:56:51 +0900

    202512301356

--- my-flask/routes/user.py
+++ my-flask/routes/user.py
@@ -2,3 +2,6 @@ from flask import Blueprint, jsonify, request
 
 bp = Blueprint('user', __name__, url_prefix='/user')
 
+@bp.route('/list', methods=['GET'])
+def user_list():
+    return jsonify({'users': ['user1', 'user2', 'user3']})
