fcdebe8bd7a8e93882ace9197447da29e8343e69
semiconduct-bsw <chem1bsw.fac@gmail.com>
2025-12-30 13:51:15 +0900

    202512301351

--- my-flask/routes/__init__.py
+++ my-flask/routes/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask
-from . import iris_view, main_view, test
+from . import iris_view, main_view, test, user
 
 def register_routes(app: Flask):
     app.register_blueprint(iris_view.bp)
     app.register_blueprint(main_view.bp)
     app.register_blueprint(test.bp)
+    app.register_blueprint(user.bp)
