7f28e5b06b2531d4fb4bb02af0c470bd402479ed
semiconduct-bsw <chem1bsw.fac@gmail.com>
2026-01-06 15:56:09 +0900

    202601061556

--- my-flask/routes/main_view.py
+++ my-flask/routes/main_view.py
@@ -3,6 +3,14 @@ from flask import Blueprint, render_template
 # Blueprint 생성
 bp = Blueprint('main', __name__)
 
+@bp.route('/layout')
+def layout():
+    return render_template('layout.html')
+
+@bp.route('/id-class')
+def id_class():
+    return render_template('id-class.html')
+
 @bp.route('/')
 def index():
     return render_template('index.html')
